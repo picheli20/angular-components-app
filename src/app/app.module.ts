3844c97d1b27d0baf9cea41ae8afa40b4fad7c26
Fabio Picheli <fabio.picheli@betsys.com>
2018-10-26 13:35:44 +0200

    remove lib depency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { CounterModule } from '../../node_modules/angular-components-lib/dist/counter';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +9,6 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    CounterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
