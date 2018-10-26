d3e420adfd2950ae78b1f726e1cebdbd187c296d
Fabio Picheli <fabio.picheli@betsys.com>
2018-10-26 12:18:38 +0200

    feat: add more complex example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CounterModule } from '../../node_modules/angular-components-lib/dist/counter';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CounterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
